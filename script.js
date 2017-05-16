a25ab6375e94afacf510f8a35eea18c1d774c6bf
RaedKhalaf1142118 <Raed.khalaf@exalt.ps>
2017-05-16 16:02:44 +0300

    adding_comments

--- script.js
+++ script.js
@@ -1,8 +1,92 @@
 var initailState;
-onload = function(){
-	setInterpolations(); // read the interpolation once the page loaded
+
+// Data //
+var model = {
+	firstName : "Raed",
+	lastName : "Khalaf",
+	age : 19,
+	// this model has two data sections
+	// the first is the actual data
+	// the second is the viewed data
+	viewed_model : {
+		viewed_firstName : "{{firstName}}",
+		viewed_lastName : "{{lastName}}",
+		viewed_age : "{{age}}"
+	}
 };
 
+onload = function(){
+	setInterpolations(); // read the interpolation once the page loaded
+	var event = new Event("onTurnDone");
+};
+
+
+/*
+* the method that starts the change detection system
+*/
+function changeDetectionSystem(interpolatedElements){
+	// operate the detection system over all the interpolated elements 
+	for (var i = 0; i < interpolatedElements.length; i++) {
+		operateChangeSystem(interpolatedElements[i]);
+	};
+}
+
+/*
+* return the interpolation of an element as an array
+*/
+function getInterpolations(element){
+	return Array.from(element.outerHTML.match(/\{\{\w{0,}\}\}/g));
+}
+
+/*
+* operate the change detection system on each element that has the
+*
+*/
+function operateChangeSystem(element){
+	// get the interpolations occured in this element
+	var interpolations = getInterpolations(element);
+	var changed = false;
+	// pass through all the interpolations and check if there is 
+	// any change occures
+	for (var i = 0; i < interpolations.length; i++) {
+		if(checkChanges(extractVariable(interpolations[i]))){
+			changed = true;
+			break;
+		}
+	};
+
+	if(changed)// if any change happend
+		updateElement(element,interpolations);// update this element
+}
+
+/*
+* update this element according to the model object
+*/
+function updateElement(element, interpolations){
+	for (var i = 0; i < interpolations.length; i++) {
+		var variable = extractVariable(interpolations[i]);
+		element.outerHTML = element.outerHTML.replace(interpolations[i],this.model[variable]);
+	};
+}
+
+/*
+* returns the variable name mentioned in the interpolation
+*/
+function extractVariable(interpolation){
+	return interpolation.substring(2,interpolation.length-2);
+}
+
+/*
+* check if there is any changes between the viewed data and the actual data
+*/
+function checkChanges(property){
+	return (model[property] != model.viewed_model["viewed_"+property]);
+}
+
+/*
+* read all the interpolation inside the document
+* and remember them
+*/
 function setInterpolations(){
 	/*
 	* save the root element which is the body
@@ -21,9 +105,9 @@ function setInterpolations(){
 	*/
 	getInterpolateElements(rootElement, interpolatedElements);
 
-	zone.run(()=>{
-		console.log(interpolatedElements);
-	});
+	document.addEventListener("onTurnDone", changeDetectionSystem(interpolatedElements));
+	//document.dispatchEvent(event);
+	console.log(interpolatedElements);
 }
 
 /*
